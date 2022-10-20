2f88f6205da910174bfc026ab74c13bb2cc86bb2
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-20 15:32:04 +0000

    user defined function compiles

--- Matrix Handout/main.cpp
+++ Matrix Handout/main.cpp
@@ -2,8 +2,20 @@
 // CSCI 130
 // 10-20-22
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+void printMatrix_3col(int matrix[][3], int N_ROWS, int N_COLUMNS){
+
+int row, col;
+    for (row = 0; row < N_ROWS; row++)
+    {
+        for(col = 0; col < N_COLUMNS; col++)
+            cout << setw(6) << matrix[row][col] << " ";
+        
+        cout << endl;    
+    }
+  }
 
 int main(){
   
