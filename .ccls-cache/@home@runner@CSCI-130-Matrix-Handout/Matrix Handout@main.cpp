506ad34b974e2174ce2927d4b49fdb400c1703d6
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-24 15:17:21 +0000

    added the new function

--- .ccls-cache/@home@runner@CSCI-130-Matrix-Handout/Matrix Handout@main.cpp
+++ .ccls-cache/@home@runner@CSCI-130-Matrix-Handout/Matrix Handout@main.cpp
@@ -17,6 +17,18 @@ int row, col;
     }
   }
 
+int CountUnhealthySensitive(int matrix[][3], int N_ROWS, int city_col){
+
+  int count = 0; 
+  int unhealthy_level = 101;
+  int row;
+  
+  for (row = 0; matrix[row][city_col] >= unhealthy_level; row++){
+    count ++;
+      return count;
+    }
+}
+
 int main(){
 
   int AirQuality_Index[10][3] = {{50, 68, 58}, {58, 115, 78}, {62, 110, 81}, {66, 124, 110}, {64, 152, 121}, {46, 143, 90}, {46, 143, 90}, {25, 91, 91}, {27, 19, 59}, {17, 13, 27}};
