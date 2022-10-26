9546f1fcd090d252f4d80907a84397251814b9a0
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-26 07:15:16 +0000

    finally got row_avg to work

--- .ccls-cache/@home@runner@Bronson-7-9/Bronson 7-9@main.cpp
+++ .ccls-cache/@home@runner@Bronson-7-9/Bronson 7-9@main.cpp
@@ -19,20 +19,18 @@ int row, col;
 
 // CODE PROVIDED BY Biruk Abebe
 // CODE ADAPTED BY Samuel Peel
-int row_avg(int numCols, int numRows){
+int row_avg(int list[][7], int numCols, int numRows){
 
   int sum, avgR;
-  int list[numCols][numRows];
-  sum = 0;
 
 //average of a row
 for (int i = 0; i < numCols; i++)//iterate each row(one loop for the row)
 {
-    //sum=0;//reset sum for each row
-    for (int j = 0; j < numRows; j++)//iterate each column in each row(one loop for the column)
-            sum += list[i][j];//will sum up the contents of the current row
-
-    avgR=sum/numCols;//give you the average of each row
+    sum=0;//reset sum for each row
+    for (int j = 0; j < numRows; j++){//iterate each column in each row(one loop for the column)
+            sum = sum + list[i][j];//will sum up the contents of the current row
+  }
+    avgR = sum/numRows;//give you the average of each row
   return avgR;
 }
   }
@@ -56,7 +54,7 @@ int main() {
 
   
 
-  int finalGrades[60][7] = {{100, 100, 100, 100, row_avg(1, 4), weightedAverage(finalGrades, 4, 1)}, {100, 0, 100, 0}, {82, 94, 73, 86}, {64, 74, 84, 94}, {94, 84, 74, 64}};
+  int finalGrades[60][7] = {{100, 100, 100, 100, row_avg(finalGrades, 1, 4), weightedAverage(finalGrades, 4, 1)}, {100, 0, 100, 0}, {82, 94, 73, 86}, {64, 74, 84, 94}, {94, 84, 74, 64}};
 
   printMatrix(finalGrades, 60, 7);
 
