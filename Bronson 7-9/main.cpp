08c6eba58aaedb6742bee83440c201a4edee6c34
Aero186 <111770122+Aero186@users.noreply.github.com>
2022-10-26 06:36:21 +0000

    didnt even use a matrix last time, (dumb)

--- Bronson 7-9/main.cpp
+++ Bronson 7-9/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void printMatrix(int matrix[5][5], int N_ROWS, int N_COLUMNS){
+void printMatrix(int matrix[][7], int N_ROWS, int N_COLUMNS){
 
 int row, col;
     for (row = 0; row < N_ROWS; row++)
@@ -17,72 +17,38 @@ int row, col;
     }
   }
 
-void average(int list[], int N_ROWS){
+int average(int matrix[][7], int N_ROWS, int student){
 
 int sum = 0;
 
 for (int i = 0; i < N_ROWS; i++) {
-    sum = (sum + list[i]) ;
+    sum = (sum + matrix[student][i]) ;
   }
-  sum = sum /N_ROWS; 
-  cout << sum << endl;
+  avg = sum /N_ROWS; 
+  return avg;
   }
 
-void weightedAverage(int list[], int N_ROWS){
+int weightedAverage(int matrix[][7], int N_ROWS, int student){
 
   int sum = 0;
 
-  sum = ((0.2*list[1]) + (0.3*list[2]) + (0.3*list[3]) + (0.2*list[4]));
+  sum = ((0.2*matrix[student][1]) + (0.3*matrix[student][2]) + (0.3*matrix[student][3]) + (0.2*matrix[student][4]));
 
-  cout << sum << endl;
+  return sum;
 }
 
 
 int main() {
 
-  int grade[5][5] = {{1, 100, 100, 100, 100}, {2, 100, 0, 100, 0}, {3, 82, 94, 73, 86}, {4, 64, 74, 84, 94}, {5, 94, 84, 74, 64}};
+  int grade[5][5] = {{100, 100, 100, 100}, {100, 0, 100, 0}, {82, 94, 73, 86}, {64, 74, 84, 94}, {94, 84, 74, 64}};
 
-  printMatrix(grade, 5, 5);
 
   cout << endl;
 
-  cout << "Student 1 Average: \n";
-  int student1[4] = {100, 100, 100, 100};
-  average(student1, 4);
-
-    cout << endl;
   
-  cout << "Student 2 Average: \n";
-  int student2[4] = {100, 0, 100, 0};
-  average(student2, 4);
 
-    cout << endl;
-  
-  cout << "Student 3 Average: \n";
-  int student3[4] = {82, 94, 73, 86};
-  average(student3, 4);
+  int finalGrades[60][7] = {{100, 100, 100, 100,  average(finalGrades, 4, 1), weightedAverage(finalGrades, 4, 1)}, {100, 0, 100, 0}, {82, 94, 73, 86}, {64, 74, 84, 94}, {94, 84, 74, 64}};
 
-    cout << endl;
-  
-  cout << "Student 4 Average: \n";
-  int student4[4] = {94, 84, 74, 64};
-  average(student4, 4);
+  printMatrix(finalGrades, 60, 7);
 
-    cout << endl;
-
-  cout << "Student 1 Weighted Average: \n";
-  weightedAverage(student1, 4);
-  cout << endl;
-
-  cout << "Student 2 Weighted Average: \n";
-  weightedAverage(student2, 4);
-  cout << endl;
-
-  cout << "Student 3 Weighted Average: \n";
-  weightedAverage(student3, 4);
-  cout << endl;
-
-  cout << "Student 4 Weighted Average: \n";
-  weightedAverage(student4, 4);
-  cout << endl;
 }
